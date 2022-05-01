408d82e974317409f84334ac266bb9d27c24d60a
septimiuProdanAdminTools <65479793+EdyEgo@users.noreply.github.com>
2022-05-01 11:59:08 +0300

    update

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,13 +1,60 @@
 
 <script setup lang="ts">
+import {ref,onMounted,reactive} from 'vue'
+import {useOptions} from '../stores/options'
+ 
+
+const optionsStore = useOptions()
+
+const rowsNumber = ref(21)
+const columnsNumber = ref(32)
+
+
+// {[key:number]:{[key:number]:{wall:string,bomb:string}}}
+const columnsWithObstacles = reactive
+<any>({
+  
+  // ex rowNumber:columnNumber: 
+  //co1:{1:{wall:'if there is a wall then add here a style',bomb:false,}
+    "10":{"5":{wall:'bg-black',bomb:''}}
+})
+
+
+
+onMounted(()=>{
+ 
+   
+ if(screen.width > 790){
+      rowsNumber.value = 28
+      columnsNumber.value = 75
+ }
+})
+
+
+function paintColumn(indexRow:number,indexColumn:number){
+  
+    const indexRowString = indexRow.toString()
+    const indexColumnString = indexColumn.toString()
+
+      console.log('ce plm','type',columnsWithObstacles[indexRowString],'aa',)
+  if(columnsWithObstacles[indexRowString] !== undefined && columnsWithObstacles[indexRowString][indexColumnString] !== undefined){
+      return columnsWithObstacles[indexRowString][indexColumnString].wall
+  }
+  return ''
+
+return columnsWithObstacles[1][1].wall
+
+}
+
+//columnsWithObstacles[indexRow][indexColumn] || ''
 
 </script>
 
 <template>
         <table class="w-[97%]">
                 <tbody>
-                <tr class="row 28" v-for="index in 28">
-                  <td class="border border-[#C572FF] p-[1%] square 75" v-for="index in 75">
+                <tr :class="`row-${indexRow}`"  v-for="indexRow in rowsNumber">
+                  <td class="border  border-[#C572FF] p-[1%] " :class="[`column-${indexColumn}`,paintColumn(indexRow, indexColumn)]" v-for="indexColumn in columnsNumber">
                    
                   </td>
                 </tr>
@@ -17,8 +64,5 @@
 
 <style>
 
-table{
-    padding: 0 0;
-    margin: 0 0 ;
-}
+
 </style>
---
+++ src/stores/options.ts
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+
+
+export const useOptions = defineStore({
+  id: 'blackLists',
+
+  state: () => {
+    return <
+      {
+       wallStatus:boolean
+       trapStatus:boolean
+      }
+    >{
+      wallStatus:false,
+      trapStatus:false
+    }
+  },
+
+  persist: true,
+
+  getters: {},
+
+  actions: {
+  
+    
+    commandStateWall(wallState:boolean){
+      this.wallStatus = wallState
+    },
+    commandTrapStatus(bombState:boolean){
+      this.trapStatus = bombState
+    }
+  }
+})
